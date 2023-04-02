836975b5175bcd9b8e820b7cf16b9686516af7e8
majed <magedelayh098@gmail.com>
2023-04-02 05:35:28 +0300

    delete folder te

--- main/views.py
+++ main/views.py
@@ -15,47 +15,48 @@ import os
 def homePage(request):
     
     message = None
-    if request.method == "POST":
-        if "image" in request.FILES:
-            image = request.FILES["image"]
-            print(image,"::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::")
+    message = "Ender Test"
+    # if request.method == "POST":
+    #     if "image" in request.FILES:
+    #         image = request.FILES["image"]
+    #         print(image,"::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::")
 
-            if not image.name.endswith('.bin'):
-                return HttpResponse('Error: Invalid file type. Please upload a .bin file.')
+    #         if not image.name.endswith('.bin'):
+    #             return HttpResponse('Error: Invalid file type. Please upload a .bin file.')
 
 
-            # Specify the directory where you want to save the uploaded file
-            directory = "te/.pio/build/esp32dev/"
-            if not os.path.exists(directory):
-                os.makedirs(directory)
+    #         # Specify the directory where you want to save the uploaded file
+    #         directory = "te/.pio/build/esp32dev/"
+    #         if not os.path.exists(directory):
+    #             os.makedirs(directory)
                 
-            # Check if a file with the same name already exists and replace it
-            file_path = os.path.join(directory, image.name)
-            if os.path.exists(file_path):
-                os.remove(file_path)
+    #         # Check if a file with the same name already exists and replace it
+    #         file_path = os.path.join(directory, image.name)
+    #         if os.path.exists(file_path):
+    #             os.remove(file_path)
                 
-            with open(file_path, 'wb+') as destination:
-                for chunk in image.chunks():
-                    destination.write(chunk)
+    #         with open(file_path, 'wb+') as destination:
+    #             for chunk in image.chunks():
+    #                 destination.write(chunk)
 
 
-            # Change to the directory where your firmware project is located
-            os.chdir('/home/engmaged/Desktop/Django/Photo/ESP_upload/')
+    #         # Change to the directory where your firmware project is located
+    #         os.chdir('/home/engmaged/Desktop/Django/Photo/ESP_upload/')
 
-            # # Build and upload the firmware to ESP32
-            # os.system('platformio run --target upload')        
-            # 
+    #         # # Build and upload the firmware to ESP32
+    #         # os.system('platformio run --target upload')        
+    #         # 
 
-            try:
-                output = subprocess.check_output(['platformio', 'run', '--target', 'upload'], cwd='te')
-                print(output.decode('utf-8'),"RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR")
-                message = output.decode('utf-8').replace('\n', '<br>')
-            except subprocess.CalledProcessError as e:
-                print(e.output.decode('utf-8'),"ssssssssssssssssssssssssssssssssssssss")
-                message = e.output.decode('utf-8').replace('\n', '<br>')
+    #         try:
+    #             output = subprocess.check_output(['platformio', 'run', '--target', 'upload'], cwd='te')
+    #             print(output.decode('utf-8'),"RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR")
+    #             message = output.decode('utf-8').replace('\n', '<br>')
+    #         except subprocess.CalledProcessError as e:
+    #             print(e.output.decode('utf-8'),"ssssssssssssssssssssssssssssssssssssss")
+    #             message = e.output.decode('utf-8').replace('\n', '<br>')
 
-        else:
-            image = None
+    #     else:
+    #         image = None
 
 
        
--- te/upload_firmware.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# Change to the directory where your firmware project is located
-os.chdir('/home/sky/Documents/PlatformIO/Projects/te')
-
-# Build and upload the firmware to ESP32
-os.system('platformio run --target upload')
-
